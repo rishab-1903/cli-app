edc1299e840372c94fc1c10e25d55256b73531e5
rishab-1903 <rishabgupta2003@gmail.com>
2025-01-11 00:23:19 +0530

    cli_python for http req #1

---
+++ cli_app.py
@@ -0,0 +1,24 @@
+import aiohttp
+import asyncio
+import argparse
+
+async def fetch_url(session, url):
+    try:
+        async with session.get(url) as response:
+            content = await response.text()
+            print(f"URL: {url} | Status: {response.status} | Content Length: {len(content)}")
+    except Exception as e:
+        print(f"Failed to fetch {url}: {e}")
+
+async def main(urls):
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch_url(session, url) for url in urls]
+        await asyncio.gather(*tasks)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Async http request with CLI")
+    parser.add_argument("urls", metavar="URL", nargs="+", help="One or more URLs to fetch")
+    args=parser.parse_args()
+
+
+    asyncio.run(main(args.urls))
